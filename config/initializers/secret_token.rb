f876d314e9cfe223620fd4fdc211fc89f30b1802
Fabian Dittrich <fdi@betterplace.org>
2011-01-19 11:46:32 +0100

    before adding compass

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CincopalabritasCom::Application.config.secret_token = '78d563fac60f8b62d81fce383b094817e0bd9959fccfb713952588148b37fa38807bcd3232d019e7da5c50dddf76fa3a9188350e6e9e3085df190aca2dec1139'
