cd24ecb7ee179089812ae7e0f5c96e9e914b665b
Fabian Dittrich <fdi@betterplace.org>
2011-01-28 18:08:06 +0100

    gfd

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource_or_scope)
     if resource_or_scope.is_a?(User)
-      user_root_path(resource_or_scope)
+      user_root_path()
     else
       super
     end
