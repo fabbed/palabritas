1c5c41e3dc1b7c39dbcd3d144d591b85722e9d55
Fabian Dittrich <fdi@betterplace.org>
2011-02-02 16:11:24 +0100

    gfddsgfds

--- app/controllers/word_sets_controller.rb
+++ app/controllers/word_sets_controller.rb
@@ -9,9 +9,9 @@ class WordSetsController < ApplicationController
   def update
     @user = User.find(params[:user_id])
 
-    if params[:word_set][:reply]
+    if params[:word_set][:comment]
       @word_set = WordSet.find(:first, :conditions => ['receiver_id = ? AND id = ?', @user.id, params[:id]])
-      @word_set.comments.create(:title => "", :comment => params[:word_set][:reply], :user_id => current_user.id)
+      @word_set.comments.create(:title => "", :comment => params[:word_set][:comment], :user_id => current_user.id)
       UserMailer.new_comment_on_your_word_set(@word_set).deliver if @word_set.sender
       redirect_to show_user_path(@user.username)
     end
