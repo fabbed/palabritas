20f6430f96553ff3b8372de536201cfb005c8108
Fabian Dittrich <fdi@betterplace.org>
2011-02-04 15:35:38 +0000

    hgfdhgfdhfdg

--- app/controllers/word_sets_controller.rb
+++ app/controllers/word_sets_controller.rb
@@ -43,16 +43,16 @@ class WordSetsController < ApplicationController
       end
 
 
-      if @user.auth_type == "facebook"
-        fb_user = FbGraph::User.me(@user.last_access_token).fetch
-        fb_user.feed!(
-          :message => I18n.t("facebook.wall_post_for_receiver_after_receiving_words.message", :sender => (current_user ? current_user.username : "Anónimo"), :receiver => @user.username, :words => params[:word_set][:words].inject([]){|words,hash| words << hash[1]}.join(",")),
-          :picture => HOST + @user.avatar.url(:medium),
-          :link => HOST + @user.username,
-          :name => I18n.t("facebook.wall_post_for_receiver_after_receiving_words.name"),
-          :description => I18n.t("facebook.wall_post_for_receiver_after_receiving_words.description")
-        )        
-      end
+      # if @user.auth_type == "facebook"
+      #   fb_user = FbGraph::User.me(@user.last_access_token).fetch
+      #   fb_user.feed!(
+      #     :message => I18n.t("facebook.wall_post_for_receiver_after_receiving_words.message", :sender => (current_user ? current_user.username : "Anónimo"), :receiver => @user.username, :words => params[:word_set][:words].inject([]){|words,hash| words << hash[1]}.join(",")),
+      #     :picture => HOST + @user.avatar.url(:medium),
+      #     :link => HOST + @user.username,
+      #     :name => I18n.t("facebook.wall_post_for_receiver_after_receiving_words.name"),
+      #     :description => I18n.t("facebook.wall_post_for_receiver_after_receiving_words.description")
+      #   )        
+      # end
 
 
 
